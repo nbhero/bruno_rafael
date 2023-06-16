fedd17ca65600bbf07b73eb7b43ee34474a00144
brunorafael72 <brunorafael72@outlook.com>
2023-06-15 21:13:03 -0300

    Adding of the Main Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/navbar'
 
 function App() {
 
   return (
     <>
       <div>
+        <Navbar />
         <h1>Hello World!</h1>
       </div>
     </>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+import styles from '../components_style/Navbar.module.css'
+
+function Navbar() {
+  return (
+    <nav className={styles.menu}>
+        <ul>
+            <li className={styles.menu_list}>
+                <a href='#' className={styles.menu_list_items}>Home</a>
+                <a href='#'className={styles.menu_list_items}>Contato</a>
+                <a href='#'className={styles.menu_list_items}>Sobre</a>
+            </li>
+        </ul>
+                
+    </nav>
+  )
+}
+
+export default Navbar
